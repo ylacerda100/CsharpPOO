58a4396537eb7d635b09a863495a44f304a7622d
Ygor Lacerda <ylacerda100@gmail.com>
2021-01-15 19:24:16 -0300

    nova classe

---
+++ 05-ByteBank/Cliente.cs
@@ -0,0 +1,9 @@
+namespace _05_ByteBank
+{
+    public class Cliente
+{
+    public string nome;
+    public string cpf;
+    public string profissao;
+}
+}
---
+++ 05-ByteBank/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _05_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente c1 = new Cliente();
+            c1.nome = "Gabriela";
+            c1.cpf = "123.456.789-10";
+            c1.profissao = "Advogada";
+
+
+            ContaCorrente conta1 = new ContaCorrente();
+            conta1.titular = c1;
+            
+        }
+    }
+}

386e7aaa88fc161b57454ff197ef8c22eeddf02e
ygors <ygors@DESKTOP-746LT0F>
2021-01-20 12:40:57 -0300

    ...

--- 03-ByteBank/obj/Release/net5.0/03-ByteBank.AssemblyInfo.cs
+++ 03-ByteBank/obj/Release/net5.0/03-ByteBank.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("03-ByteBank")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("03-ByteBank")]
--- 05-ByteBank/obj/Release/net5.0/05-ByteBank.AssemblyInfo.cs
+++ 05-ByteBank/obj/Release/net5.0/05-ByteBank.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("03-ByteBank")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("05-ByteBank")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("03-ByteBank")]
-[assembly: System.Reflection.AssemblyTitleAttribute("03-ByteBank")]
+[assembly: System.Reflection.AssemblyProductAttribute("05-ByteBank")]
+[assembly: System.Reflection.AssemblyTitleAttribute("05-ByteBank")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
